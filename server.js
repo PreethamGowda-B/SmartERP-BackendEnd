ecb5c1a2f831d2ca22182d5a1cf3249619649224
preethu <thepreethu01@gmail.com>
2026-02-05 13:02:00 +0530

    fix: Add PATCH method to CORS allowed methods for messages API

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(
       callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     exposedHeaders: ["Set-Cookie"],
     optionsSuccessStatus: 200,

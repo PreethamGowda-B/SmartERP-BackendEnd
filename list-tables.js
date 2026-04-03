2ab830cf1bc13f2b8c6874fa1f649351defd9b9c
PreethamGowda-B <thepreethu01@gmail.com>
2026-04-03 20:53:47 +0530

    chore: remove 80+ junk files - debug scripts, check scripts, test runners, migration runners, log files, one-off SQL files

--- list-tables.js
+++
@@ -1,7 +0,0 @@
-const { pool } = require('./db');
-async function list() {
-  const r = await pool.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public'");
-  console.table(r.rows);
-  process.exit(0);
-}
-list();

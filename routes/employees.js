6624f05d9953b8a196c929f57c602c9c150ee5c1
PreethamGowda-B <thepreethu01@gmail.com>
2026-08-01 13:56:10 +0530

    fix(employees): imported body and validationResult from express-validator in routes/employees.js

--- routes/employees.js
+++ routes/employees.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
 const { pool } = require('../db');
 
 const { authenticateToken } = require('../middleware/authMiddleware');

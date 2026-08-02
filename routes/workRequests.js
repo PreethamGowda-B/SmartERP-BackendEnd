dd6d3e108997f2927d67a00db9348661a3431ce8
PreethamGowda-B <thepreethu01@gmail.com>
2026-08-02 21:09:52 +0530

    fix(imports): fix authMiddleware import path in routes/workRequests.js

--- routes/workRequests.js
+++ routes/workRequests.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const { authenticateToken } = require('../middleware/auth');
+const { authenticateToken } = require('../middleware/authMiddleware');
 const { createNotificationForOwners, createNotification } = require('../services/notificationHelper');
 const invoiceService = require('../services/invoiceService');
 

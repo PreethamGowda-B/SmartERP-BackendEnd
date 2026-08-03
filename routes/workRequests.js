bbca2bdb2822b7ab05f1fb8a8fd5a6479499ad84
PreethamGowda-B <thepreethu01@gmail.com>
2026-08-03 19:49:47 +0530

    fix(db): destructure pool in routes/workRequests.js to fix 500 error

--- routes/workRequests.js
+++ routes/workRequests.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const { pool } = require('../db');
 const { authenticateToken } = require('../middleware/authMiddleware');
 const { createNotificationForOwners, createNotification } = require('../utils/notificationHelpers');
 const invoiceService = require('../services/invoiceService');
